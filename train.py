8791e602a00c53f8255bc0ea868f7a4c179731dc
Huy Huynh <49454101+huytjuh@users.noreply.github.com>
2022-01-31 13:40:39 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,15 @@
 import time
+import pandas as pd
+
 from options.train_options import TrainOptions
 
+
+if __name__ == '__main__':
+  opt = TrainOptions().parse()          # training options
+  dataset = create_dataset(opt)         # initialize and preprocess dataset
+  dataset_size = len(dataset)           
+  print('Size: %d' % dataset_size)
+  
+  model = create_model(opt)             # create a model given opt.model and other options
+  model.setup(opt)
+  
